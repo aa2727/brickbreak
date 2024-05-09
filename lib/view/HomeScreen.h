ba509921e262ee1f9542036b64f232308f507f0d
Antoine Lemaitre <lemaitreantoine2711@gmail.com>
2024-05-09 07:09:45 +0200

    Fenetre premiere

--- lib/view/HomeScreen.h
+++ lib/view/HomeScreen.h
@@ -5,6 +5,7 @@
 #include <SDL2/SDL_ttf.h>
 #include <SDL2/SDL_image.h>
 #include <memory>
+#include <array>
 #include <string>
 #include <iostream>
 
@@ -29,9 +30,13 @@ private:
     std::unique_ptr<SDL_Window,decltype(&SDL_DestroyWindow)> window;
     std::unique_ptr<SDL_Renderer,decltype(&SDL_DestroyRenderer)> renderer;
     std::unique_ptr<SDL_Texture,decltype(&SDL_DestroyTexture)> background;
+
     std::unique_ptr<SDL_Texture,decltype(&SDL_DestroyTexture)> title;
+    SDL_Rect titleRectSrc, titleRectDest;
     std::unique_ptr<SDL_Texture,decltype(&SDL_DestroyTexture)> playButton;
+    SDL_Rect playButtonRectSrc, playButtonRectDest;
     std::unique_ptr<SDL_Texture,decltype(&SDL_DestroyTexture)> quitButton;
+    SDL_Rect quitButtonRectSrc,quitButtonRectDest;
 
 };
 
