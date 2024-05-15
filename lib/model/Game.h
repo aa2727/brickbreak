5e5be146657ae3a108572466695229324ab676be
Antoine Lemaitre <lemaitreantoine2711@gmail.com>
2024-05-15 05:42:32 +0200

    Objet Game

--- lib/model/Game.h
+++ lib/model/Game.h
@@ -1,4 +1,37 @@
 #ifndef GAME_H
 #define GAME_H
 
+#include "model/items/platform/Platform.h"
+#include "model/items/ball/Ball.h"
+#include "model/items/wall/Wall.h"
+#include "model/items/grid/Grid.h"
+
+#define PLATFORM_POS_X 255.0
+#define PLATFORM_POS_Y 480.0
+#define BALL_SPEED 1.
+
+class Game
+{
+public:
+    Game();
+    Game(const int width, const int height);
+    ~Game();
+    void init();
+    void update();
+    bool is_end();
+
+    bool balls_are_out();
+    void handle_collision();
+
+private:
+    int max_width;
+    int max_height;
+    std::shared_ptr<Platform> plat;
+    std::unique_ptr<Grid> grid;
+    std::vector<std::unique_ptr<Wall>> walls;
+    std::vector<std::unique_ptr<Ball>> balls;
+};
+
+
+
 #endif // GAME_H
---
+++ src/model/Game.cpp
@@ -0,0 +1,77 @@
+#include "model/Game.h"
+
+// Constructors
+Game::Game()
+{
+    Game(640, 480);
+}
+
+Game::Game(const int width, const int height) : walls(), 
+                                                balls(),
+                                                plat(nullptr)
+
+{
+    this->max_width = width;
+    this->max_height = height;
+}
+
+// Destructor
+
+Game::~Game()
+{
+}
+
+// Methods
+
+void Game::init()
+{
+    this->plat = std::make_shared<Platform>(PLATFORM_POS_X, PLATFORM_POS_Y);
+    this->grid = std::make_unique<Grid>(0, 0, this->max_width, this->max_height / 4, 6, 6);
+    this->walls = std::vector<std::unique_ptr<Wall>>(3);
+    this->balls = std::vector<std::unique_ptr<Ball>>(1);
+    this->walls.push_back(std::make_unique<Wall>(0, 0, this->max_width, 10));
+    this->walls.push_back(std::make_unique<Wall>(0, 0, 10, this->max_height));
+    this->walls.push_back(std::make_unique<Wall>(this->max_width - 10, 0, 10, this->max_height));
+    this->balls.push_back(std::make_unique<Ball>(255., 400., 1, -1.5, 10));
+}
+
+void Game::update()
+{
+    this->plat->movement();
+    for (auto &ball : this->balls)
+    {
+        ball->move(BALL_SPEED);
+    }
+    this->handle_collision();
+}
+
+bool Game::is_end()
+{
+    return this->balls_are_out() || this->grid->is_empty();
+}
+
+bool Game::balls_are_out()
+{
+    for (auto &ball : this->balls)
+    {
+        if (ball->get_position().at(1) < this->max_height)
+        {
+            return false;
+        }
+    }
+    return true;
+}
+
+void Game::handle_collision()
+{
+    for(auto it = this->balls.begin(); it != this->balls.end(); ++it)
+    {
+        (*it)->resolve_collision(*this->plat);
+        (*it)->resolve_collision(*this->grid);
+        for(auto it2 = this->walls.begin(); it2 != this->walls.end(); ++it2)
+        {
+           (*it)->resolve_collision(**it2);
+        }
+
+    }
+}
